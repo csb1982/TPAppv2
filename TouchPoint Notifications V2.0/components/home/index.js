35c216c765161cfbae7cbf59235ebe7bf0689c8d
Craig Bentall <craig.bentall@eclipselegal.co.uk>
2016-10-24 12:46:22 +0000

    Changes made by Craig Bentall (craig.bentall@eclipselegal.co.uk).
    Changes made to iframe so that it's full screen

--- TouchPoint Notifications V2.0/components/home/index.js
+++ TouchPoint Notifications V2.0/components/home/index.js
@@ -1,10 +1,20 @@
 'use strict';
 
 app.home = kendo.observable({
-    onShow: function () { },
+    onShow: function () {
+        // Show form
+        $("#tpForm").show();
+     },
     afterShow: function () { }
 });
 (function (parent) {
+
+    // On orientation change re set iframe height
+    $(window).on("orientationchange",function(){
+    var h = $(".km-content").height();
+    $("#tpFrame").height(h);
+    });
+
     var homeModel = kendo.observable({
         fields: {
             rememberMe: '',
@@ -13,8 +23,19 @@ app.home = kendo.observable({
             username: '',
         },
         submit: function () {
+            // Hardcoded values for form
+            $("#userID").val("qatesting331");
+            $("#site").val("qatesting331");
+
+            // Hide form
+            $("#tpForm").hide();
+
+            // Set iframe's height. So that it's full screen
+            var h = $(".km-content").height();
+            $("#tpFrame").height(h);
+
             // Submit form
-            $("#tpForm").submit();
+            $("#tpForm").submit();                     
         },
         cancel: function () { }
     });
